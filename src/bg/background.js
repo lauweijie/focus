d75501a17ae24e44caba970e6ad696c70fdf9cc3
Jonathan Lau <jon@jon.sg>
2015-02-13 13:42:51 +0800

    Use chrome.storage to persist data

--- src/bg/background.js
+++ src/bg/background.js
@@ -1,12 +1,8 @@
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-	// console.log('tabs.onUpdated');
-	// console.log(tabId, changeInfo, tab);
 	update();
 });
 
 chrome.tabs.onActivated.addListener(function(activeInfo) {
-	// console.log('tabs.onActivated');
-	// console.log(activeInfo);
 	update();
 });
 
@@ -14,19 +10,54 @@ chrome.windows.onFocusChanged.addListener(function(windowId) {
 	update();
 });
 
-var sites = [''];
+chrome.tabs.onRemoved.addListener(function(tabId, removeInfo) {
+	update();
+});
+
+chrome.alarms.onAlarm.addListener(function(alarm){
+	update();
+});
+
+chrome.alarms.create('', {periodInMinutes: 1});
+
+var prevUpdate;
+
+var storage = [];
 
 var update = function() {
 	console.log('Update triggered.');
+
+	if(prevUpdate) {
+		var update = prevUpdate;
+		prevUpdate = null;
+		var timeDiff = Math.floor((new Date() - update.time) / 1000);
+		if(timeDiff > 0) {
+			chrome.storage.local.get('usage', function(items) {
+				var data = items.usage;
+				if(typeof data !== 'object') {
+					console.log('asd');
+					data = new Object();
+				}
+				data[update.host] = (data[update.host] || 0) + timeDiff;
+				chrome.storage.local.set({'usage': data});
+				console.log(data);
+			});
+		}
+	}
+
 	chrome.tabs.query({active: true}, function(tabs) {
     tabs.forEach(function(tab) {
       chrome.windows.get(tab.windowId, function(window) {
         if(window.focused) {
         	// tab is active and window is focused
-        	console.log(tab.url);
         	var parser = document.createElement('a');
         	parser.href = tab.url;
-        	console.log(parser.hostname);
+
+        	prevUpdate = {
+        		time: new Date(),
+        		host: parser.hostname
+        	};
+
         }
       });
     });
